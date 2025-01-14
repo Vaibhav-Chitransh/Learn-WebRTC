852ac3ae710cee5b9887d579a5cdd77db90e5a28
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2025-01-14 22:39:14 +0530

    implemented video chat using peerjs

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ io.on('connection', (socket) => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
         socket.broadcast.emit('user-connected', userId);
+
+        socket.on('disconnect', () => {
+          socket.broadcast.emit('user-disconnected', userId);
+        })
     })
 })
 
